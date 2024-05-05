0ece0c4e053162bd429ec611fd5f2190130514d7
DustinWin <45238096+DustinWin@users.noreply.github.com>
2024-05-06 03:25:50 +0800

    优化转换脚本

--- tools/convert.sh
+++ tools/convert.sh
@@ -19,15 +19,18 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'PROCESS-PATH,' | sed 's/^PROCESS-PATH,//' > ${list[i]}/path.json
 	fi
 	# domain
+ 	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN,')" ]; then
+		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN,' | sed 's/^DOMAIN,//' > ${list[i]}/domain.json
+	fi
+ 	# suffix
 	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-SUFFIX,')" ]; then
 		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-SUFFIX,' | sed 's/^DOMAIN-SUFFIX,//' > ${list[i]}/suffix.json
 	fi
-	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN,')" ]; then
-		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN,' | sed 's/^DOMAIN,//' > ${list[i]}/domain.json
-	fi
+ 	# keyword
 	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-KEYWORD,')" ]; then
 		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-KEYWORD,' | sed 's/^DOMAIN-KEYWORD,//' > ${list[i]}/keyword.json
 	fi
+ 	# regex
  	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-REGEX,')" ]; then
 		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'DOMAIN-REGEX,' | sed 's/^DOMAIN-REGEX,//' > ${list[i]}/regex.json
 	fi
@@ -35,7 +38,7 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 	if [ -n "$(cat ./rules/${list[i]}/${list[i]}.yaml | grep 'IP-CIDR')" ]; then
 		cat ./rules/${list[i]}/${list[i]}.yaml | grep 'IP-CIDR' | sed -e 's/^IP-CIDR,//' -e 's/^IP-CIDR6,//' > ${list[i]}/ipcidr.json
 	fi
-	# 转成json格式
+	# 转成 .json 格式
 	# android package
 	if [ -f "${list[i]}/package.json" ]; then
 		sed -i 's/.*/        "&",/' ${list[i]}/package.json
@@ -60,16 +63,19 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 		sed -i '1s/^/      "domain": [\n/' ${list[i]}/domain.json
 		sed -i '$s/,$/\n      ],/' ${list[i]}/domain.json
 	fi
+ 	# suffix
 	if [ -f "${list[i]}/suffix.json" ]; then
 		sed -i 's/.*/        "&",/' ${list[i]}/suffix.json
 		sed -i '1s/^/      "domain_suffix": [\n/' ${list[i]}/suffix.json
 		sed -i '$s/,$/\n      ],/' ${list[i]}/suffix.json
 	fi
+ 	# keyword
 	if [ -f "${list[i]}/keyword.json" ]; then
 		sed -i 's/.*/        "&",/' ${list[i]}/keyword.json
 		sed -i '1s/^/      "domain_keyword": [\n/' ${list[i]}/keyword.json
 		sed -i '$s/,$/\n      ],/' ${list[i]}/keyword.json
 	fi
+ 	# regex
  	if [ -f "${list[i]}/regex.json" ]; then
 		sed -i 's/.*/        "&",/' ${list[i]}/regex.json
 		sed -i '1s/^/      "domain_regex": [\n/' ${list[i]}/regex.json
@@ -86,5 +92,6 @@ for ((i = 0; i < ${#list[@]}; i++)); do
 	sed -i '1s/^/{\n  "version": 1,\n  "rules": [\n    {\n/' ${list[i]}.json
 	sed -i '$s/,$/\n    }\n  ]\n}/' ${list[i]}.json
 	rm -rf ${list[i]}
+ 	# 转成 .srs 格式
 	./sing-box rule-set compile ${list[i]}.json -o ${list[i]}.srs
 done
